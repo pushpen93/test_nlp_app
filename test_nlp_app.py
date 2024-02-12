b2c93f26510355c18b24e828097083731a8a446e
pushpendra tawar <98085926+pushpen93@users.noreply.github.com>
2024-02-12 20:52:41 +0530

    Update test_nlp_app.py

--- test_nlp_app.py
+++ test_nlp_app.py
@@ -1,19 +1,14 @@
+import fitz
 import streamlit as st
-from pdfminer.high_level import extract_text
-import spacy
-import pytesseract
-from PIL import Image
-from io import BytesIO
-import base64
 
-# Initialize spaCy model
-nlp = spacy.load("en_core_web_sm")
-
-# Function to extract text from PDF
-def extract_text_from_pdf(pdf_file):
-    return extract_text(pdf_file)
-
-# Function to extract text from an image using OCR
+def extract_text_from_pdf_with_fitz(pdf_file):
+    # Open the PDF file
+    pdf_document = fitz.open(stream=pdf_file.read(), filetype="pdf")
+    text = ""
+    for page in pdf_document:
+        text += page.get_text()
+    pdf_document.close()
+    return text
 def extract_text_from_image(image_file):
     image = Image.open(image_file)
     return pytesseract.image_to_string(image)
@@ -37,7 +32,7 @@ if uploaded_file is not None:
 
     if file_details["filetype"] == "application/pdf":
         with st.spinner('Extracting text from PDF...'):
-            text = extract_text_from_pdf(uploaded_file)
+            text = extract_text_from_pdf_with_fitz(uploaded_file)
             st.text_area("Extracted text", text, height=300)
             extracted_info = extract_entities(text)
             st.write(extracted_info)
@@ -47,4 +42,3 @@ if uploaded_file is not None:
             st.text_area("Extracted text", text, height=300)
             extracted_info = extract_entities(text)
             st.write(extracted_info)
-
