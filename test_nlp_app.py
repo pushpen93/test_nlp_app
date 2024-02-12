38578bcfb249703146d1300933d29fb9275c2ff5
pushpendra tawar <98085926+pushpen93@users.noreply.github.com>
2024-02-12 16:52:14 +0530

    Update test_nlp_app.py

--- test_nlp_app.py
+++ test_nlp_app.py
@@ -1,3 +1,56 @@
 import streamlit
+import pandas
+import snowflake.connector
+import requests
+from urlib.error import URLError
 
-streamlit.title('hAPPY NEW YEAR')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include 
+#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(my_fruit_list)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
+
+streamlit.write('The user entered ', fruit_choice)
+
+streamlit.stop()
+
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contain:")
+streamlit.dataframe(my_data_rows)
+
+add_myfruit= streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding',add_myfruit)
+
+my_cur.execute("insert into fruit_load_list vakues ('from streamlit')")
